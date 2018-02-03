d1b31d6c6970c125defbe01122c110dfdadc4829
Marcin Zieliński <marcin.x.zielinski@gmail.com>
2018-02-03 23:44:30 +0100

    feed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -64,7 +64,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
-    assert_redirected_to root_url
+    assert_redirected_to login_url
   end
 
   test "should redirect destroy when logged in as non-admin" do
@@ -73,6 +73,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 
 end
